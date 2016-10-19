ef2911ac721ff5bc0f2bda7293d48101b561992f
snopcoff <snoopy.8994@gmail.com>
2016-10-19 06:16:43 +0000

    Profile style

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,15 +18,12 @@ class User < ActiveRecord::Base
     end
   end
   
-=begin
   def self.new_with_session(params, session)
     super.tap do |user|
-      if(data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"] || 
-        data = ["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"] || 
-        data = ["devise.gplus_data"] && session["devise.gplus_data"]["extra"]["raw_info"])
+      if(data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"])
         user.email = data["email"] if user.email.blank?
+        user.avatar = data["image"]
       end
     end
   end
-=end
 end
