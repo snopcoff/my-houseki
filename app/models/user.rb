24dbc4cc5b7795ae62e9197748da1bf0dc59dcc6
snopcoff <snoopy.8994@gmail.com>
2016-10-23 06:58:35 +0000

    Style

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook, :twitter, :google_oauth2]
   # after_create :assign_role
   mount_uploader :avatar, AvatarUploader
+  
+  has_many :fooddrinks, :dependent => :destroy
   acts_as_commontator
          
   validates :name, presence: true
