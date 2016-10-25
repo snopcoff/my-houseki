42656ab18a0fb0b39d8c2f03fb02861ddcb1c176
Linh Dang <mlinh211@gmail.com>
2016-10-25 10:55:32 +0700

    fix img + notification

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require toastr
 //= require_tree .
+
+window.onload = function reset(){
+    var notice = $('#notice');
+    if (notice.text() != ""){
+        toastr.success(notice.text());
+        notice.hide();
+    }
+
+}
