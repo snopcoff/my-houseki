aaf99b7dd9996adeb847934c72b236d762d2d8dd
snopcoff <snoopy.8994@gmail.com>
2016-11-03 12:10:38 +0000

    Display Food Drink by Rating

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,9 @@ class StaticPagesController < ApplicationController
     @users = User.all
     
     @fd_types = FdType.all
+    
+    
+    @recommendfood = Fooddrink.where(foodtype: true).order(avg: :desc)
+    @recommenddrink = Fooddrink.where(foodtype: false).order(avg: :desc)
   end
 end
